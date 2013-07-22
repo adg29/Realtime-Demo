093e05e16436f0eef9ea2f5a6e8a8df80e1b45c8
Alan David Garcia <alandavidgarcia@gmail.com>
2013-07-22 17:58:02 -0400

    Actually catch the tag endpoint for subscription and processing

--- server.js
+++ server.js
@@ -22,6 +22,48 @@ app.get('/callbacks/geo/:geoName', function(request, response){
   response.send(params['hub.challenge'] || 'No hub.challenge present');
 });
 
+app.get('/callbacks/tag/:tag', function(request, response){
+    // The GET callback for each subscription verification.
+  helpers.debug("GET " + request.url); 
+  var params = url.parse(request.url, true).query;
+  response.send(params['hub.challenge'] || 'No hub.challenge present');
+});
+
+app.post('/callbacks/tag/:tag', function(request, response){
+  helpers.debug("PUT /callbacks/tag/" + request.params.tag);
+   // The POST callback for Instagram to call every time there's an update
+   // to one of our subscriptions.
+    
+   // First, let's verify the payload's integrity
+   if(!helpers.isValidRequest(request)) {
+     helpers.debug('request validity questionable')
+     //response.send('FAIL');
+     //return;
+   }
+    
+    // Go through and process each update. Note that every update doesn't
+    // include the updated data - we use the data in the update to query
+    // the  API to get the data we want.
+  var updates = JSON.parse(request.rawBody);
+  console.log(request.rawBody)
+  console.log(request.body)
+  var tag = request.params.tag;
+  helpers.debug('tag')
+  helpers.debug(tag)
+  for(index in updates){
+    var update = updates[index];
+    helpers.debug('updateLoop')
+    helpers.debug(update)
+    if(update['object'] == "geography"){
+      helpers.processGeography(geoName, update);
+    }
+    else if(update['object'] == "tag"){
+      helpers.processTag(tag, update);
+    }
+  }
+  response.send('OK');
+});
+
 app.post('/callbacks/geo/:geoName', function(request, response){
   helpers.debug("PUT /callbacks/geo/" + request.params.geoName);
    // The POST callback for Instagram to call every time there's an update
@@ -29,15 +71,17 @@ app.post('/callbacks/geo/:geoName', function(request, response){
     
    // First, let's verify the payload's integrity
    if(!helpers.isValidRequest(request)) {
-     helpers.debug('FAIL')
-     response.send('FAIL');
-     return;
+     helpers.debug('request validity questionable')
+     //response.send('FAIL');
+     //return;
    }
     
     // Go through and process each update. Note that every update doesn't
     // include the updated data - we use the data in the update to query
     // the  API to get the data we want.
   var updates = JSON.parse(request.rawBody);
+  console.log(request.rawBody)
+  console.log(request.body)
   var geoName = request.params.geoName;
   helpers.debug('geoNameParams')
   helpers.debug(geoName)
@@ -45,10 +89,13 @@ app.post('/callbacks/geo/:geoName', function(request, response){
     var update = updates[index];
     helpers.debug('updateLoop')
     helpers.debug(update)
-    if(update['object'] == "geography")
+    if(update['object'] == "geography"){
       helpers.processGeography(geoName, update);
+    }
+    else if(update['object'] == "tag"){
+      helpers.processTag(geoName, update);
+    }
   }
-  helpers.debug("Processed " + updates.length + " updates");
   response.send('OK');
 });
 
